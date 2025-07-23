d46674545adbe54aba8a54f40dfc58f5bc6dc37a
BuildTools <unconfigured@null.spigotmc.org>
2025-07-22 19:54:38 -0500

    issue was we were checking all the evenys already but now game keeps up

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from tile import Tile
 from board import Board
 from player import Player
+from game_manager import GameManager
 
 
 # encapsulate the display dimensions and board size at some point
@@ -16,33 +17,32 @@ screen = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
 
 # allow specific events
-pygame.event.set_allowed([pygame.MOUSEBUTTONDOWN])
+# pygame.event.set_allowed([pygame.MOUSEBUTTONDOWN])
 
 running = True
 board = Board(width=board_size[0], height=board_size[1], screen=screen)
 # player will handle all the states related to player 
 player = Player()
+game_manager = GameManager()
 
-def handle_mouse_events():
-    mouse_events = pygame.event.get(eventtype=pygame.MOUSEBUTTONDOWN)
-    if mouse_events:
-        tile : Tile = board.get_tile_at_pos(mouse_events[0].pos[0],mouse_events[0].pos[1])
+
+def handle_mouse_clicks(event):
+    if event.button == 1:
+        tile : Tile = board.get_tile_at_pos(event.pos[0],event.pos[1])
         player.change_selection(tile)
         
-
+screen.fill("white") 
+board.draw_board(screen)
 
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            handle_mouse_clicks(event)
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill("white")  # Clear the screen with black
    
     # draw entities onto the screen
     board.draw_board(screen)
-
-    # event handler or sytems
-    handle_mouse_events()
 
     pygame.display.flip()
     # clock.tick(480)  # Limit to 480 frames per second
