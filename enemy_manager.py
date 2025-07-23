2a228e33d1074e33ba633a483a2c0e66b62eb30a
BuildTools <unconfigured@null.spigotmc.org>
2025-07-22 19:57:31 -0500

    next goal is to add enemy and make move

--- enemy_manager.py
+++ enemy_manager.py
@@ -1,5 +1,7 @@
 
-
+# should manage the enemies and the locations
+# different thing sto consider but necessary now
+# ideally spawn an enemy not sure if that should be handled by the game board but this class should handle smooth motion for the enemy ensuring constant speed
 class EnemyManager:
     def __init__(self, spawn_points: list[tuple]):
         # spawn points are indexed by row and column of the tile rather then screen position
