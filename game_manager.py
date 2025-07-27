d7fed145aef111b113a3619a7a3d5802b96707e4
BuildTools <unconfigured@null.spigotmc.org>
2025-07-27 09:41:23 -0500

    rudimentary button working, need a better way to handle ui elements vs game elements when clicking

--- game_manager.py
+++ game_manager.py
@@ -1,4 +1,13 @@
+from enum import Enum
 
+class GameState(Enum):
+    # will probably control the screen currently being displayed
+    START = 1
+    PLAYING = 2
+    PAUSED = 3
+    GAME_OVER = 4
+    VICTORY = 5
+    SETTINGS = 6
 
 class GameManager:
     def __init__(self, tile_size):
